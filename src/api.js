3f20024782585cf1609deb0d3afafff4cc58bb44
guyn123 <quanhhth492005@gmail.com>
2025-07-23 08:54:05 +0700

    v10

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.62.104:8084/api',
+  baseURL: 'http://172.17.154.71:8084/api',
 });
 
 api.interceptors.request.use(async (config) => {
